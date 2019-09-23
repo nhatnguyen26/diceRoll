1175fe99a20e0288f93e0625976aa0d09ac3fc8d
nhatnguyen <nhatnguyen@ebay.com>
2019-09-23 11:14:55 -0700

    my version of dicee

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'dart:io';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blueAccent[100],
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.blueAccent[100],
         ),
         body: DicePage(),
       ),
@@ -15,9 +17,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  DicePageState createState() {
+    // TODO: implement createState
+    return DicePageState();
+  }
+}
+
+class DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
+  Random random = Random();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: diceClick,
+              child: Image.asset('images/dice$leftDice.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: diceClick,
+              child: Image.asset('images/dice$rightDice.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void diceClick() {
+    setState(() {
+      leftDice = random.nextInt(6) + 1;
+      rightDice = random.nextInt(6) + 1;
+    });
   }
 }
